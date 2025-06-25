005e9c424d80ae3b37e7d1c3002044095cf07aeb
daffaagustians <dafagustiansaadi12@gmail.com>
2025-06-25 22:48:27 +0700

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 // const port = 3000;
 
 // Middleware buat parse JSON
+app.use(cors());
 app.use(express.json());
 
 // Array buat nyimpan to-do
